877fbe0e4c4619d7fb99f4275ae41c4526b9f0db
gtgong <gtgong@scsol.co.kr>
2022-06-08 17:51:32 +0900

    feat: 데이터 전송 작업 진행

--- src/main/java/com/devGong/client/sockets/ClientSocket.java
+++ src/main/java/com/devGong/client/sockets/ClientSocket.java
@@ -22,9 +22,13 @@ public class ClientSocket {
         int key;
 
         StringBuilder stringBuilder = new StringBuilder();
+        /* PRE-INSTALL(31byte) / SETTING / 2:REQUEST / 3:REPORT(141byte) / 4:DATA
+
+
+         */
 
         do {
-            System.out.println("0: PRE-INSTALL / 1:SETTING / 2:REQUEST / 3:REPORT / 4:DATA ");
+            System.out.println("Input => 0: PRE-INSTALL(31byte) / 1:SETTING / 2:REQUEST / 3:REPORT / 4:DATA / 8: ACK / 9: NAK  ");
             key = scanner.nextInt();
 
             switch (key) {
@@ -32,51 +36,90 @@ public class ClientSocket {
                 case 0:
                     System.out.println("PRE-INSTALL selected");
                     for (int i = 0; i < messageLength; i++) {
+                        stringBuilder.append("flag");
                         stringBuilder.append("000000000000000");
                         stringBuilder.append("00NONE");
+                        stringBuilder.append("yyyymmddhhmmss");
                         stringBuilder.append("0xFFFF");
                     }
                     break;
-               /* case 1:
+                case 1:
                     System.out.println("SETTING selected ");
                     for (int i = 0; i < messageLength; i++) {
-                        stringBuilder.append("a1");
-                        stringBuilder.append("b1");
-                        stringBuilder.append("c1");
-                        stringBuilder.append("d1");
-                        stringBuilder.append("e1");
+                        stringBuilder.append("RecordingTime1");
+                        stringBuilder.append("RecordingTime2");
+                        stringBuilder.append("RecordingTime3");
+                        stringBuilder.append("S/N");
+                        stringBuilder.append("period");
+                        stringBuilder.append("SamplingTime");
+                        stringBuilder.append("ServreURL");
+                        stringBuilder.append("ServerPort");
+                        stringBuilder.append("CheckSum");
                     }
                     break;
                 case 2:
                     System.out.println("REQUEST selected ");
                     for (int i = 0; i < messageLength; i++) {
-                        stringBuilder.append("a2");
-                        stringBuilder.append("b2");
-                        stringBuilder.append("c2");
-                        stringBuilder.append("d2");
-                        stringBuilder.append("e2");
+                        stringBuilder.append("DebugMessage");
+                        stringBuilder.append("RecordingTime1");
+                        stringBuilder.append("RecordingTime2");
+                        stringBuilder.append("RecordingTime3");
+                        stringBuilder.append("FirmwareVersion");
+                        stringBuilder.append("BatteryVoltage");
+                        stringBuilder.append("ModernRSSI");
+                        stringBuilder.append("Project");
+                        stringBuilder.append("SID");
+                        stringBuilder.append("Period");
+                        stringBuilder.append("ServerURL");
+                        stringBuilder.append("ServerPort");
+                        stringBuilder.append("CheckSUM");
+
                     }
                     break;
                 case 3:
                     System.out.println("REPORT selected ");
                     for (int i = 0; i < messageLength; i++) {
-                        stringBuilder.append("a3");
-                        stringBuilder.append("b3");
-                        stringBuilder.append("c3");
-                        stringBuilder.append("d3");
-                        stringBuilder.append("e3");
+                        stringBuilder.append("RecordingTime1");
+                        stringBuilder.append("RecordingTime2");
+                        stringBuilder.append("RecordingTime3");
+                        stringBuilder.append("Sleep");
+                        stringBuilder.append("Period");
+                        stringBuilder.append("SamplingTime");
+                        stringBuilder.append("F-reset");
+                        stringBuilder.append("Px");
+                        stringBuilder.append("Py");
+                        stringBuilder.append("Active");
+                        stringBuilder.append("SampleRate");
+                        stringBuilder.append("ServerURL");
+                        stringBuilder.append("ServerPort");
+                        stringBuilder.append("CheckSUM");
                     }
                     break;
                 case 4:
                     System.out.println("DATA selected ");
                     for (int i = 0; i < messageLength; i++) {
-                        stringBuilder.append("a4");
-                        stringBuilder.append("b4");
-                        stringBuilder.append("c4");
-                        stringBuilder.append("d4");
-                        stringBuilder.append("e4");
+                        stringBuilder.append("EndRecordTime");
+                        stringBuilder.append("RecordingTime1");
+                        stringBuilder.append("RecordingTime2");
+                        stringBuilder.append("RecordingTime3");
+                        stringBuilder.append("FirmwareVersion");
+                        stringBuilder.append("BatteryVoltage");
+                        stringBuilder.append("ModernRSSI");
+                        stringBuilder.append("DeviceStatus");
+                        stringBuilder.append("SamplingTime");
+                        stringBuilder.append("Px");
+                        stringBuilder.append("Py");
+                        stringBuilder.append("Period");
+                        stringBuilder.append("ServerURL");
+                        stringBuilder.append("ServerPort");
+                        stringBuilder.append("Sleep");
+                        stringBuilder.append("Active");
+                        stringBuilder.append("F-reset");
+                        stringBuilder.append("SampleRate");
+                        stringBuilder.append("CregCount");
+                        stringBuilder.append("CheckSum");
                     }
-                    break;*/
+                    break;
             }
 
         }
@@ -86,19 +129,27 @@ public class ClientSocket {
 
         byte[] totalData = stringBuilder.toString().getBytes();
 
-        System.out.println("Sending message");
+//        System.out.println("<<<Sending message>>>");
 
         try {
             OutputStream os = socket.getOutputStream();
 
-            for (int i = 0; i < messageLength / delimiterLength; i++) {
+         /*   for (int i = 0; i < messageLength / delimiterLength; i++) {  //delimiterLength = 256
                 byte[] sending = Arrays.copyOfRange(totalData, i * delimiterLength, (i + 1) * delimiterLength);
                 //               Arrays.copyOfRange(원본 배열, 복사할 시작인덱스, 복사할 끝인덱스) 인덱스는 0부터 시작하는것 기준
                 System.out.println("sending... " + (i + 1));
                 os.write(sending); //write( byte[] sending ) 매개값으로 주어진 바이트 배열의 모든 바이트를 출력 스트림으로 보냅니다
                 os.flush(); // flush()는 버퍼에 남아있는 데이터를 모두 출력시키고, 버퍼를 비우는 역할을 합니다.
                 Thread.sleep(500);
-            }
+            }*/
+            byte[] sending = Arrays.copyOf(totalData, messageLength);
+            //byte[] sending = totalData ;
+
+            os.write(sending); //write( byte[] sending ) 매개값으로 주어진 바이트 배열의 모든 바이트를 출력 스트림으로 보냅니다
+            os.flush(); // flush()는 버퍼에 남아있는 데이터를 모두 출력시키고, 버퍼를 비우는 역할을 합니다.
+            Thread.sleep(500);
+
+
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
