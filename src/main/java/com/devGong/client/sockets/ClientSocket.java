9b1688c80b3220322a676775081ada1ee4cc88e1
gtgong <gtgong@scsol.co.kr>
2022-08-01 13:52:40 +0900

    feat : 기존 프로토콜 기반 preinstall 작업완료

--- src/main/java/com/devGong/client/sockets/ClientSocket.java
+++ src/main/java/com/devGong/client/sockets/ClientSocket.java
@@ -17,28 +17,6 @@ public class ClientSocket {
     private Socket socket;
     static Scanner sc = new Scanner(System.in);
 
-    public byte[] makeLocation(String px, String py) {
-
-        // 점 제거 + 10진수로 변환하기   String -> decimal
-        int convertedPx = Integer.parseInt(px.replace(".", ""));  //1272411553
-        int convertedPy = Integer.parseInt(py.replace(".", "")); //37315906
-
-        // 16진수로 변환하기   decimal -> hexa
-        String first = Integer.toHexString(convertedPx); //4bd775a1
-        String second = Integer.toHexString(convertedPy);// 2396542
-        System.out.println("[String/first] : " + first);
-        System.out.println("[String/second] : " + second);
-
-        //바이트 배열에다가 헥사값 두개를 담아서 그걸 리턴해야함
-        byte[] totalByte = new byte[2];
-        byte[] firstByte = new BigInteger(first, 16).toByteArray();
-        byte[] secondByte = new BigInteger(second, 16).toByteArray();
-
-        totalByte[0] = firstByte[0];
-        totalByte[1] = secondByte[0];
-
-        return totalByte;
-    }
 
     public byte[] makeChecksum(String totalData) {
         int total = 0;
@@ -116,41 +94,43 @@ public class ClientSocket {
             stringBuilder.append("00"); //debug message,  number   변동사항 거의 있을수 있음. 2
 
 
-            byte[] preinstallTotalData = stringBuilder.toString().getBytes();
+            byte[] totalData = stringBuilder.toString().getBytes();
 
-            System.out.println("totalData " + new String(preinstallTotalData));
+            System.out.println("totalData " + new String(totalData));
 
             try {
                 os = socket.getOutputStream();
-                os.write(preinstallTotalData);
+                os.write(totalData);
                 os.write(makeChecksum(stringBuilder.toString()));
                 os.flush();
-
-            } catch (IOException e) {
+                Thread.sleep(500);
+            } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
             }
 
-            System.out.println("[ PreInstall Receiving message ]");
+            System.out.println("===[ PreInstall Receiving message ]===");
 
             try {
                 is = socket.getInputStream();
 
-                byte[] reply = new byte[200];
+                byte[] reply = new byte[3000];
+                System.out.println("reply-->" + new String(reply));
+                System.out.println("length-->" + reply.length);
+
 
                 if (is.read(reply) < 0)
                     throw new SocketException();
                 // 넘겨 받은 값을 찍어줌
                 System.out.println(" [Receive preinstall INFO] : " + new String(reply));
-
-                /* reply가 맞는 값이 왔다면 ACK or NAK 와 report 값을 서버에 보내줌! */
                 System.out.println("=== [PreInstall_REPORT PROCESS START ] ===");
 
                 if (reply != null) {    // 서버에서 날라온 값이 있을 경우, ACK을 Flag로 가져감.
                     /*============ 저장공간 리셋 ========*/
-                    Arrays.fill(preinstallTotalData, (byte) 0);   //  pre-install 값 담긴 바이트배열  0으로 초기화.
-                    System.out.println("[totalData reset] : " + new String(preinstallTotalData));
+                    Arrays.fill(totalData, (byte) 0);   //  pre-install 값 담긴 바이트배열  0으로 초기화.
+                    System.out.println("[totalData reset] : " + new String(totalData));
+                    System.out.println("stringBuilder-->" + stringBuilder);
                     stringBuilder.setLength(0); // stringBuilder를 초기화.
-
+                    System.out.println("stringBuilder reset -->" + stringBuilder);
                     /*============ Header ============*/
                     stringBuilder.append("8");  // Flag
                     stringBuilder.append("SWSLB-20220530-0000-7877");  // SerialNum
@@ -165,12 +145,10 @@ public class ClientSocket {
                     stringBuilder.append("L7.300");   // Firmware Version | char 6
                     stringBuilder.append("3.590");   // Battery Voltage | char 6
                     stringBuilder.append(1);   // Modem RSSI | number 1
-
                     stringBuilder.append("00"); //device status | char 2
                     stringBuilder.append(3);   // Sampling Time | number 1
                     stringBuilder.append("128.593085");//Px | char 10
                     stringBuilder.append("35.845037 ");//Py | char 10
-
                     stringBuilder.append("821227203543");//Pname(Phone) | char 16
                     stringBuilder.append("producttest     ");   // SID |char 16
                     stringBuilder.append(5);   // Period number 1
@@ -183,71 +161,97 @@ public class ClientSocket {
                     stringBuilder.append("0");// baudrate next | char 1
                     stringBuilder.append(2);// pcb version | number 1
 
-                    preinstallTotalData = stringBuilder.toString().getBytes();
-                    String report = new String(preinstallTotalData);
+                    totalData = stringBuilder.toString().getBytes();
+                    String report = new String(totalData);
                     System.out.println("[report(D->S)]-->" + report);
 
                     try {
                         os = socket.getOutputStream();
-                        os.write(preinstallTotalData);
+                        os.write(totalData);
                         os.flush();
-                        //보내고 reply를 초기화시키고 거기다가 report result를 받아야함
-                        Arrays.fill(reply, (byte) 0);   //  pre-install 값 담긴 바이트배열  0으로 초기화.
+                        Thread.sleep(500);
+                        Arrays.fill(reply, (byte) 0);
 
-                        byte[] resultArr = new byte[1];
-                        stringBuilder.setLength(0); // stringBuilder를 초기화.
+                        byte[] test = new byte[1];
+                        stringBuilder.setLength(0);
 
                         is = socket.getInputStream();
-                        if (is.read(resultArr) < 0)
-
-                            //시발것
+                        if (is.read(test) < 0)
                             throw new SocketException();
 
-                        System.out.println("[ACK/NAK Result] : " + new String(resultArr));
-                        System.out.println("[ACK/NAK Result] : " + Arrays.toString(resultArr));
-                    } catch (IOException e) {
+                        System.out.println("[ACK/NAK Result] : " + new String(test));
+                    } catch (InterruptedException | IOException e) {
                         e.printStackTrace();
                     }
+
+                    // ACK ---> setting process 타야함
+
+
+
+
 
                 } else {
                     // 서버에서 날라온 값이 없을 경우, NAK(9)을 Flag로 가져감.
                     /*============ 저장공간 리셋 ========*/
-                    Arrays.fill(preinstallTotalData, (byte) 0);  //  pre-install 값 담긴 바이트배열  0으로 초기화.
+                    Arrays.fill(totalData, (byte) 0);  //  pre-install 값 담긴 바이트배열  0으로 초기화.
                     stringBuilder.setLength(0); // stringBuilder를 초기화
                     /*============ Header ============*/
                     stringBuilder.append("9");  // Flag
-                    stringBuilder.append("SWSLB-20220530-0000-0001");  // SerialNum
-                    stringBuilder.append("20200101 000014");  // DataTime
+                    stringBuilder.append("SWSLB-20220530-0000-7877");  // SerialNum
+                    stringBuilder.append("20200101 000014");  // DateTime
                     stringBuilder.append("00");  // paraLen
                     /*============ Body ============*/
-                    stringBuilder.append("00");   // DebugMessage
-                    stringBuilder.append("0200");   // RecordingTime1
-                    stringBuilder.append("0300");   // RecordingTime2
-                    stringBuilder.append("0400");   // RecordingTime3
-                    stringBuilder.append("L7.300");   // Firmware Version
-                    stringBuilder.append("3.5900");   // Battery Voltage
-                    stringBuilder.append("0x5b");   // Modem RSSIhttps://www.hankyung.com/realestate/article/202206231960i
-                    stringBuilder.append("0x03");   // Sampling Time
-                    stringBuilder.append("4");   // SampleRate
-                    stringBuilder.append("862785043595621");   // Modem Number
-                    stringBuilder.append("test_hkchoi");   // Project
-                    stringBuilder.append("producttest");   // SID
-                    stringBuilder.append("60");   // Period
-                    stringBuilder.append("thingsware.co.kr");   // Server URL
-                    stringBuilder.append("6669");   // Server Port
-                    stringBuilder.append("274a");   // CheckSum
+                    stringBuilder.append("00");   // DebugMessage | char 13    --->>>>  1에서 13으로 변함
+                    stringBuilder.append("0200");   // RecordingTime1 | char 4
+                    stringBuilder.append("0300");   // RecordingTime2 | char 4
+                    stringBuilder.append("0400");   // RecordingTime3 | char 4
+                    stringBuilder.append("0959");   // FM Radio | char 4
+                    stringBuilder.append("L7.300");   // Firmware Version | char 6
+                    stringBuilder.append("3.590");   // Battery Voltage | char 6
+                    stringBuilder.append(1);   // Modem RSSI | number 1
+                    stringBuilder.append("00"); //device status | char 2
+                    stringBuilder.append(3);   // Sampling Time | number 1
+                    stringBuilder.append("128.593085");//Px | char 10
+                    stringBuilder.append("35.845037 ");//Py | char 10
+                    stringBuilder.append("821227203543");//Pname(Phone) | char 16
+                    stringBuilder.append("producttest     ");   // SID |char 16
+                    stringBuilder.append(5);   // Period number 1
+                    stringBuilder.append("thingsware.co.kr                ");   // Server URL   32
+                    stringBuilder.append("6669");   // Server Port  4
+                    stringBuilder.append("thingsware.co.kr                ");// DB URL | char 32
+                    stringBuilder.append("3306");// DB PORT | char 4
+                    stringBuilder.append(3);// radio time | number 1
+                    stringBuilder.append("0");// baudrate | char 1
+                    stringBuilder.append("0");// baudrate next | char 1
+                    stringBuilder.append(2);// pcb version | number 1
 
-                    preinstallTotalData = stringBuilder.toString().getBytes();
+                    totalData = stringBuilder.toString().getBytes();
+                    String report = new String(totalData);
+                    System.out.println("[report(D->S)]-->" + report);
+
+                    totalData = stringBuilder.toString().getBytes();
 
                     try {
                         os = socket.getOutputStream();
-                        os.write(preinstallTotalData);
+                        os.write(totalData);
                         os.flush();
                         Thread.sleep(500);
+                        Arrays.fill(reply, (byte) 0);
 
+                        byte[] test = new byte[1];
+                        stringBuilder.setLength(0);
+
+                        is = socket.getInputStream();
+                        if (is.read(test) < 0)
+                            throw new SocketException();
+
+                        System.out.println("[ACK/NAK Result] : " + new String(test));
                     } catch (InterruptedException | IOException e) {
                         e.printStackTrace();
                     }
+                    // NAK ---> 다시 프리인스톨 처음 단계로 돌아감.
+
+
                 }
             } catch (IOException e) {
                 e.printStackTrace();
